13b7c7992fec5bd31dc4718246242dedf63b5a9d
quanns12 <ldat8516@gmail.com>
2025-09-12 17:29:54 +0700

    delete thêm

--- src/routes/privateRoutes/privateRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const privateRoutes = () => {
-  const user = JSON.parse(localStorage.getItem("user"));
-  return <div>privateRoutes</div>;
-};
-
-export default privateRoutes;
--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const path = {
-  // PUBLIC
-  ABOUT: "/about",
-  CONTACT: "/contact",
-  POST: "/post",
-  DETAIL: "/postdetail/:id",
-  LOGIN: "/login",
-  REGISTER: "/register",
-};
